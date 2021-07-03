806c6e39ecc4a103b47f70d4263a339170b951e1
mohira <shukatsudaison@gmail.com>
2021-07-03 23:01:33 +0900

    ch01: 飛ばし飛ばしで第1章ざっくり写経

---
+++ ch1/1.7/server2/main.go
@@ -0,0 +1,35 @@
+// 最小限の「echo」とカウンタ機能を持つサーバ
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+
+	err := http.ListenAndServe("localhost:8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
